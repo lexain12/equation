24e41c679318f66fc4157214ff0a9a4bcce27405
lexain12 <lexain12@gmail.com>
2022-08-18 16:14:41 +0300

    Update main.cpp
    now working if a == 0

--- main.cpp
+++ main.cpp
@@ -13,33 +13,45 @@ float findDiscriminant(int a, int b, int c)
 
 int main()
 {
-  int a;
-  int b;
-  int c;
+  float a;
+  float b;
+  float c;
   float discriminant;
 
   printf("Введите коэфиценты уравнения a*x^2 + b*x + c = 0\n");
-  scanf("%d %d %d", &a, &b, &c);
-
-  discriminant = findDiscriminant(a, b, c); 
-  printf("Дискриминат равен: %.3f \n", discriminant);
-
-  if (discriminant < 0)
-    printf("Нет решений");
-  else if (discriminant == 0)
+  scanf("%f %f %f", &a, &b, &c);
+  if (a == 0)
   {
-    printf("Одно решение: x = %.3f", (-b/2.0)/a);
-  } else 
+    if (b == 0)
     {
-    float x1;
-    float x2;
-
-    x1 = (-b + sqrt(discriminant)) / 2.0 / a; 
-    x2 = (-b - sqrt(discriminant)) / 2.0 / a;
-
-    printf("Уравнение имеет два решения: х1 = %.3f, x2 = %.3f", x1, x2);
+      if (c == 0)
+        printf("Бесконечное множество корней");
+      else
+        printf("Корней нет");
     }
+    else
+      printf("Единственный корень: x = %.3f", -c/b);  
+  } else
+  {
+    discriminant = findDiscriminant(a, b, c); 
+    printf("Дискриминат равен: %.3f \n", discriminant);
 
+    if (discriminant < 0)
+      printf("Нет решений");
+    else if (discriminant == 0)
+    {
+      printf("Одно решение: x = %.3f", (-b/2.0)/a);
+    } else 
+      {
+      float x1;
+      float x2;
+
+      x1 = (-b + sqrt(discriminant)) / 2.0 / a; 
+      x2 = (-b - sqrt(discriminant)) / 2.0 / a;
+
+      printf("Уравнение имеет два решения: х1 = %.3f, x2 = %.3f", x1, x2);
+      }
+  }
   
   return 0;
 }
