40ec877551666d05c8bafc9efa4cafa8c3521333
lexain12 <lexain12@gmail.com>
2022-08-19 00:44:40 +0300

    Update main.cpp
    Mega super update

--- main.cpp
+++ main.cpp
@@ -6,18 +6,26 @@ float findDiscriminant(int a, int b, int c)
   return b * b - 4 * a * c; 
 }
 
-int main()
+float* input()
 {
-  float a;
-  float b;
-  float c;
-  float discriminant;
+  float a = NAN;
+  float b = NAN;
+  float c = NAN;
 
   printf("Введите коэфиценты уравнения a*x^2 + b*x + c = 0\n");
-  scanf("%f %f %f", &a, &b, &c);
+  scanf("%f %f %f", &a, &b, &c); 
 
-  if (a == 0)
-  {
+  float *arg = (float*)malloc(sizeof(float)*3);
+
+  arg[0] = a;
+  arg[1] = b;
+  arg[2] = c;
+
+  return arg; 
+}
+
+void linealEquation(float b, float c)
+{
     if (b == 0)
     {
       if (c == 0)
@@ -27,10 +35,11 @@ int main()
     }
     else
       printf("Единственный корень: x = %.3f\n", -c / b);  
-  } 
-  else
-  {
-    discriminant = findDiscriminant(a, b, c); 
+}
+
+void quadraticEquation(float a, float b, float c)
+{
+    float discriminant = findDiscriminant(a, b, c); 
 
     if (discriminant < 0)
     {
@@ -45,16 +54,42 @@ int main()
     else 
     {
 
-    float x1;
-    float x2;
+    float x1 = NAN;
+    float x2 = NAN;
 
-    printf("Дискриминант равен: %f\n", discriminant);
+    printf("Дискриминант равен: %f\n", sqrt(discriminant));
 
     x1 = (-b + sqrt(discriminant)) / (2 * a); 
     x2 = (-b - sqrt(discriminant)) / (2 * a);
 
     printf("Уравнение имеет два решения: х1 = %.3f, x2 = %.3f\n", x1, x2);
     }
+}
+
+int main()
+{
+  float a = NAN;
+  float b = NAN;
+  float c = NAN;
+  float discriminant = NAN;
+  float* arg = NULL;
+
+  
+  arg = input();
+  a = arg[0];
+  b = arg[1];
+  c = arg[2];
+
+  free(arg);
+
+
+  if (a == 0)
+  {
+    linealEquation(b, c);
+  } 
+  else
+  {
+    quadraticEquation(a, b, c);
   }
   
   return 0;
