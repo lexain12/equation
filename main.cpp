46c539e8a2ca88271a336f44eabef9dd90fb6089
lexain12 <lexain12@gmail.com>
2022-08-25 16:01:52 +0300

    added arguments input

--- main.cpp
+++ main.cpp
@@ -4,33 +4,106 @@
 
 #include "equation.h"
 
-int main()
+bool strIsEqual(const char rightString[], char yourString[]);
+
+enum functionStrIsEqualStatus 
 {
-  int status = noErrors;
+  normalStatus = 0,
+  debugStatus = 1,
+  errorStatus = 2
+};
 
-  Coefficients coefficients = {};
-  status = coefficientInput(&coefficients);
-  if (status)
+functionStrIsEqualStatus checkArg(int numberOfArguments, char* argumentsOfCmd[])
+{
+  for (int i = 1; i < numberOfArguments; i++)
   {
-    printError(status);
-    return EXIT_FAILURE;
+    if (strIsEqual("-test", argumentsOfCmd[i]))
+    {
+      return debugStatus;
+    }  
+    else
+    {
+      printf("Undefined argument %s\n", argumentsOfCmd[i]);
+      return errorStatus;
+    }
   }
 
-  Solution solution = {};
-  status = solveQuadraticEquation(&coefficients, &solution);
-  if (status)
+  return normalStatus;
+}
+
+bool strIsEqual(const char rightString[],char yourString[])
+{
+  int i = 0;
+
+  for (; rightString[i] != '\0' && yourString[i] != '\0'; i++)
   {
-    printError(status);
-    return EXIT_FAILURE;
+    if (!(rightString[i] == yourString[i]))
+    {
+      return false;
+    }
   }
 
-  status = answerOutput(&solution);
-  if (status)
-  {
-    printError(status);
-    return EXIT_FAILURE;
-  }
+  return (rightString[i] == '\0' && yourString[i] == '\0');
+}
 
-  return 0;
+int startProgramMode(functionStrIsEqualStatus isDebug)
+{
+  switch (isDebug)
+  {
+      case debugStatus:
+        {
+        system("make debug");
+        system("./test");
+        return 0;
+        }
+
+      case normalStatus:
+      {
+        fprintf(stderr, "Normal\n");
+        int status = noErrors;
+
+        Coefficients coefficients = {};
+        status = coefficientInput(&coefficients);
+        printf("Ok\n");
+        if (status)
+        {
+          printError(status);
+          return EXIT_FAILURE;
+        }
+
+        Solution solution = {};
+        status = solveQuadraticEquation(&coefficients, &solution);
+        if (status)
+        {
+          printError(status);
+          return EXIT_FAILURE;
+        }
+
+        status = answerOutput(&solution);
+        if (status)
+        {
+          printError(status);
+          return EXIT_FAILURE;
+        }
+        return 0;
+      }
+      case errorStatus:
+      {
+        return EXIT_FAILURE;
+      }
+      default:
+      {
+        printf("default %d, isDebug = %d\n", __LINE__, isDebug);
+        return EXIT_FAILURE;
+      }
+  }
+  
+}
+
+int main(int argc, char* argv[])
+{
+  functionStrIsEqualStatus isDebug = checkArg(argc, argv);
+
+  return startProgramMode(isDebug);
 }
 
